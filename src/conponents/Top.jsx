4f22d18d9790b8af1a18c318ea87b39b16132158
Reda-Kriate <kriatereda@gmail.com>
2024-07-29 04:44:01 +0100

    commit-2nd
    time difference between Salawat

--- src/conponents/Top.jsx
+++ src/conponents/Top.jsx
@@ -6,9 +6,20 @@ import moment from "moment"
 function Tooop(){
     // console.log(moment().format("YYYY:MM:Do / hh:mm"))
     //fetching data
-    const [info,setInfo] = useState("");
-    const [city , setCity] = useState("SALE")
-const [inputVal , setInputVal] = useState("")
+    const [info,setInfo] = useState([
+        {
+            Fajr:"04:54",
+            Dhuhr:"13:39",
+            Asr:"17:19",
+            Maghrib:"20:35",
+            Isha:"22:01",
+        },
+]);
+const [city , setCity] = useState("SALE");
+const [inputVal , setInputVal] = useState("");
+const [nextPrayer , setNextPrayer] = useState();
+const [nextTimePrayer , setNextTimePrayer] = useState("");
+// const [nextPrayerIndex , setNextPrayerIndex ] = useState();
     async function getData(){
         try {
         const res = await axios.get(`https://api.aladhan.com/v1/timingsByCity?country=MA&city=${city}`);
@@ -30,6 +41,14 @@ useEffect(()=>{
     },1000);
     return ()=>{clearInterval(interval)}
 },[])
+useEffect(()=>{
+    const interval = setInterval(()=>{
+        countTime()
+    },1000)
+    return ()=>{
+        clearInterval(interval)
+    }
+},[info])
 
 function formateDate(){
     let hours = time.getHours();
@@ -51,7 +70,6 @@ function addZero(i){
 // clock end
 const handleChange=(e)=>{
     setInputVal(e.target.value)
-    
 }
 const clickChange = ()=>{
     if(inputVal){
@@ -61,15 +79,68 @@ const clickChange = ()=>{
         setCity("SALE")
     }
 }
-//calcule temps teste
-const caltime = ()=>{
-
+const countTime = ()=>{
+    const momentNow = moment();
+    // knowing what is the next prayer
+    let prayer = "";
+    if(momentNow.isAfter(moment(info["Fajr"],"hh:mm")) && 
+        momentNow.isBefore(moment(info["Dhuhr"],"hh:mm"))){
+            console.log("the next prayer is : Dhuhr")
+            setNextPrayer("Dhuhr")
+            prayer="Dhuhr"
+        }
+        else if(momentNow.isAfter(moment(info["Dhuhr"],"hh:mm")) && 
+        momentNow.isBefore(moment(info["Asr"],"hh:mm"))){
+            console.log("the next prayer is : Asr")
+            setNextPrayer("Asr")
+            prayer="Asr"
+        }
+        else if(momentNow.isAfter(moment(info["Asr"],"hh:mm")) && 
+        momentNow.isBeforeBefore(moment(info["Maghrib"],"hh:mm"))){
+            console.log("the next prayer is : Maghrib")
+            setNextPrayer("Maghrib")
+            prayer="Maghrib"
+        }
+        else if(momentNow.isAfter(moment(info["Maghrib"],"hh:mm")) && 
+        momentNow.isBefore(moment(info["Isha"],"hh:mm"))){
+            console.log("the next prayer is : Isha")
+            setNextPrayer("Isha")
+            prayer="Isha"
+        }
+        else{
+            console.log("the next prayer is : Fajr")
+            setNextPrayer("Fajr")
+            prayer="Fajr"
+        }
+        //knowing the next time prayer
+        const nextPrayerTime =info[prayer]
+            console.log(nextPrayerTime)
+        //transform the next time prayer to moment
+            const nextPrayerTimeMoment = moment(nextPrayerTime,"hh:mm")
+        //knowing time difference between salat and the current moment with millisecendes
+            let timeDifference = nextPrayerTimeMoment.diff(momentNow)
+        //condition for salat al fajr if we are before 00:00
+        if(timeDifference<0){
+            //diffence between current moment and 00:00
+            let midnightDiff = moment("23:59:59","hh:mm:ss").diff(momentNow);
+            //difference between salat fajr and 00:00
+            let fajrMidnight = nextPrayerTimeMoment.diff(moment("00:00:00","hh:mm:ss"));
+            const totalDifferenceFajr = midnightDiff + fajrMidnight;
+            timeDifference = totalDifferenceFajr
+        }
+        const timeDuration = moment.duration(timeDifference)
+        let NextTimePrayerVar = `${addZero(timeDuration.hours())}:${addZero(timeDuration.minutes())}:${addZero(timeDuration.seconds())}`;
+        setNextTimePrayer(NextTimePrayerVar)
+        console.log(NextTimePrayerVar)
 }
+
+
+
     return(<>
     <div className="big-div">
         <div className="div-one">
-            <h1>Le temps resté pour la prochaine priére </h1>
-            <p>00:00:00</p>
+            <h1>Le temps resté pour salat {nextPrayer} </h1>
+            <p>{`${nextTimePrayer}`}</p>
         </div>
         <div className="div-two">
             <h1>{formateDate()}</h1>
@@ -84,27 +155,27 @@ const caltime = ()=>{
     <div className="div-salat">
     <Salaat
     image="../src/assets/10.jpg"
-    adan="Al-Fajr"
+    adan="Fajr"
     temp={info.Fajr}
     />
     <Salaat
         image="../src/assets/20.jpg"
-        adan="Al-Dohr"
+        adan="Dhuhr"
         temp={info.Dhuhr}
     />
     <Salaat
         image="../src/assets/30.jpg"
-        adan="Al-Asr"
+        adan="Asr"
         temp={info.Asr}
     />
     <Salaat
         image="../src/assets/40.jpg"
-        adan="Al-Maghreb"
+        adan="Maghrib"
         temp={info.Maghrib}
     />
     <Salaat
         image="../src/assets/50.jpg"
-        adan="Al-Ichae"
+        adan="Isha"
         temp={info.Isha}
     />
     </div>
